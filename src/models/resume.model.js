59deedeaf9c95e4cfa91c7e93e45c35450d4d64f
mhmd-byi <mohammed@buildyourinnovation.com>
2023-12-23 01:20:36 +0530

    resume WIP

--- src/models/resume.model.js
+++ src/models/resume.model.js
@@ -65,6 +65,18 @@ const resumeSchema = mongoose.Schema(
         type: String,
         trim: true,
     },
+    bannerPhoto: {
+        type: String,
+        trim: true,
+    },
+    profilePhoto: {
+        type: String,
+        trim: true,
+    },
+    theme: {
+        type: String,
+        trim: true,
+    },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
